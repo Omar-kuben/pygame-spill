ed5174da876d1a3810502e61d7c6d69bef199fd3
omar-kuben <omhua003@osloskolen.no>
2026-01-26 10:04:52 +0100

    4 kant som beveger på seg

--- roadgame.py
+++ roadgame.py
@@ -25,27 +25,46 @@ class Ball:
         elif trykkede_taster[pg.K_RIGHT] and self._x_pos < self._vindu.get_width() - self._radius:
             self._x_pos += self._fart
 
-    
-   
+class Rektangel:
+    def __init__(self, x, y, bredde, hoyde, fart, vindu):
+        self.x = x
+        self.y = y
+        self.bredde = bredde
+        self.hoyde = hoyde
+        self.fart = fart
+        self.vindu = vindu
+
+
+
+    def tegne(self):
+        pg.draw.rect(self.vindu, (255,0,0), (self.x,self.y,self.bredde,self.hoyde)) #xpos ypos bredde høyde
+    def flytt(self):
+        if self.y <= 0  or self.y >= self.vindu.get_height() - self.hoyde: 
+            self.fart = -self.fart
+        self.y -= self.fart
 
         
- 
-VINDU_BREDDE = 500
-VINDU_HOYDE  = 500
+
+
+VINDU_BREDDE = 700
+VINDU_HOYDE  = 650
 vindu = pg.display.set_mode([VINDU_BREDDE, VINDU_HOYDE])
  
 pg.display.flip()
  
-ball = Ball(30, 255, 25, 0.1, vindu,(255,0,0))
- 
-fortsett = True
+ball = Ball(25, 350, 25, 0.1, vindu,(255,0,0))
+firekant = Rektangel(350,350,50,50,0.1,vindu)
+
+fortsett = True 
 while fortsett:
     vindu.fill((255,255,255))
     ball.tegne()
+    firekant.tegne()
     pg.display.flip()
  
     trykkede_taster = pg.key.get_pressed()
     ball.flytt(trykkede_taster)
+    firekant.flytt()
  
     for event in pg.event.get():
         if event.type == pg.QUIT:
